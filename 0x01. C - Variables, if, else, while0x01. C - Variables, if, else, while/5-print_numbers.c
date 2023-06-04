e37e08544c1ce9bf57152b1ad6d0b92f307209ff
Sibongile N <68620670+Sibongile-Nhlema@users.noreply.github.com>
2023-06-04 21:47:31 +0200

    task 5

---
+++ 0x01. C - Variables, if, else, while0x01. C - Variables, if, else, while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints single digit numbers from base 10 and \n
+ * @i: int
+ *  
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    int i = 48;
+
+    for (i = 48; i < 57; i++)
+    {
+        putchar(i);
+    }
+    putchar('\n');
+    return (0);
+}
